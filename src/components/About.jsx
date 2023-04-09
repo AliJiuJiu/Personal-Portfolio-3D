a08d8740e6ea89108ac68ff0b1553e7f5374c22a
AliJiuJiu <xiaopangengine@hotmail.com>
2023-04-09 11:54:53 +0100

    hero section finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 test-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a Senior Wind Engineer with extensive experience in a wide range of
+        wind-related fields. However, I am seeing myself more as a researcher
+        with a strong analytical mind, which allows me to identify the
+        fundamental why of a problem and solving problem part becomes natural. I
+        am also a curious mind and quick learner. I enjoy being self-taught and
+        have picked up python, pytorch, html, css, Javascript, React, Redux in
+        the past 2 years.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {/* Question: why map function needs () after arrow but won't work with {} */}
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
